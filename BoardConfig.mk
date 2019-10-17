20e165e580f4716d50a08524d6a4dfc663eebc6f
Jarl-Penguin <jarlpenguin@outlook.com>
2019-10-17 03:00:07 +0300

    pine: LineageOS bringup
    Signed-off-by: Jarl-Penguin <jarlpenguin@outlook.com>
    Change-Id: I52c34bfa26544bb238beb0890b20f7c3c0a7ada4

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -1,67 +1,82 @@
-TARGET_NO_BOOTLOADER := true
-TARGET_BOOTLOADER_BOARD_NAME := MSM8937
-TARGET_BOARD_PLATFORM := msm8937
+#
+# Copyright (C) 2019 The LineageOS Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
 
+# Architecture
 TARGET_ARCH := arm
 TARGET_ARCH_VARIANT := armv8-a
 TARGET_CPU_ABI := armeabi
 TARGET_CPU_VARIANT := cortex-a53
+TARGET_USES_64_BIT_BINDER := true
 
-# Partitions
-BOARD_BOOTIMAGE_PARTITION_SIZE := 0x4000000
-BOARD_RECOVERYIMAGE_PARTITION_SIZE := 0x4000000
-BOARD_SYSTEMIMAGE_PARTITION_SIZE := 0xc0000000
-BOARD_USERDATAIMAGE_PARTITION_SIZE := 0x2573fbe00
-BOARD_CACHEIMAGE_PARTITION_SIZE := 0x10000000
-BOARD_PERSISTIMAGE_PARTITION_SIZE := 0x2000000
-BOARD_FLASH_BLOCK_SIZE := 131072 # (BOARD_KERNEL_PAGESIZE * 64)
-BOARD_BUILD_SYSTEM_ROOT_IMAGE := true
+# Board
+TARGET_NO_BOOTLOADER := true
+TARGET_BOOTLOADER_BOARD_NAME := MSM8937
+TARGET_BOARD_PLATFORM := msm8937
+
+# Dex-preopt
+ifeq ($(HOST_OS),linux)
+  ifneq ($(TARGET_BUILD_VARIANT),eng)
+    ifeq ($(WITH_DEXPREOPT),)
+      WITH_DEXPREOPT := true
+      WITH_DEXPREOPT_BOOT_IMG_AND_SYSTEM_SERVER_ONLY := true
+    endif
+  endif
+endif
+
+# Encryption
+TARGET_HW_DISK_ENCRYPTION := true
 
 # Kernel
 BOARD_KERNEL_BASE := 0x80000000
 BOARD_KERNEL_CMDLINE := console=ttyMSM0,115200,n8
 BOARD_KERNEL_CMDLINE += androidboot.console=ttyMSM0 androidboot.hardware=qcom user_debug=30 msm_rtb.filter=0x237 ehci-hcd.park=3
 BOARD_KERNEL_CMDLINE += androidboot.bootdevice=7824900.sdhci androidboot.usbconfigfs=true androidboot.selinux=permissive androidboot.configfs=true
-BOARD_KERNEL_CMDLINE += lpm_levels.sleep_disabled=1 earlycon=msm_hsl_uart,0x78B0000 vmalloc=300M firmware_class.path=/vendor/firmware_mnt/image  loop.max_part=7 
+BOARD_KERNEL_CMDLINE += lpm_levels.sleep_disabled=1 earlycon=msm_hsl_uart,0x78B0000 vmalloc=300M firmware_class.path=/vendor/firmware_mnt/image  loop.max_part=7
 BOARD_KERNEL_PAGESIZE := 2048
 BOARD_KERNEL_TAGS_OFFSET := 0x00000100
 BOARD_RAMDISK_OFFSET := 0x01000000
-BOARD_MKBOOTIMG_ARGS := --ramdisk_offset $(BOARD_RAMDISK_OFFSET) --tags_offset $(BOARD_KERNEL_TAGS_OFFSET)
+BOARD_BOOTIMG_HEADER_VERSION := 1
+BOARD_MKBOOTIMG_ARGS := --ramdisk_offset $(BOARD_RAMDISK_OFFSET) --tags_offset $(BOARD_KERNEL_TAGS_OFFSET) --header_version $(BOARD_BOOTIMG_HEADER_VERSION)
+TARGET_KERNEL_VERSION := 4.9
+TARGET_KERNEL_ARCH := arm
+TARGET_KERNEL_HEADER_ARCH := arm
+TARGET_KERNEL_SOURCE := kernel/xiaomi/pine
+TARGET_KERNEL_CONFIG := pine-perf_defconfig
+TARGET_KERNEL_CLANG_COMPILE := true
 
-TARGET_PREBUILT_KERNEL := device/xiaomi/pine/prebuilt/kernel
-TARGET_RECOVERY_FSTAB := device/xiaomi/pine/recovery/root/etc/twrp.fstab
-TARGET_RECOVERY_PIXEL_FORMAT := "RGBX_8888"
-
-# TWRP
-TW_THEME := portrait_hdpi
-TW_NO_EXFAT := true
-TW_NO_EXFAT_FUSE := true
-TW_USE_TOOLBOX := true
-TW_USE_MODEL_HARDWARE_ID_FOR_DEVICE_ID := true
-TW_EXTRA_LANGUAGES := true
-TWRP_INCLUDE_LOGCAT := true
-TARGET_USES_LOGD := true
-
-RECOVERY_SDCARD_ON_DATA := true
+# Partitions
+BOARD_FLASH_BLOCK_SIZE := 131072 # (BOARD_KERNEL_PAGESIZE * 64)
+BOARD_BOOTIMAGE_PARTITION_SIZE := 0x4000000
+BOARD_RECOVERYIMAGE_PARTITION_SIZE := 0x4000000
+BOARD_SYSTEMIMAGE_PARTITION_SIZE := 0xc0000000
+BOARD_USERDATAIMAGE_PARTITION_SIZE := 0x2573fbe00
+BOARD_CACHEIMAGE_PARTITION_SIZE := 0x10000000
+BOARD_PERSISTIMAGE_PARTITION_SIZE := 0x2000000
+BOARD_BUILD_SYSTEM_ROOT_IMAGE := true
 
 # Qualcomm support
 BOARD_USES_QCOM_HARDWARE := true
-TARGET_RECOVERY_QCOM_RTC_FIX := true
 
 # Recovery
+TARGET_RECOVERY_FSTAB := $(DEVICE_PATH)/recovery/root/etc/twrp.fstab
 TARGET_USERIMAGES_USE_EXT4 := true
 
-# Android Verified Boot
-BOARD_AVB_ENABLE := true
+# Verity
+BOARD_AVB_ENABLE := false
+BOARD_BUILD_DISABLED_VBMETA_IMAGE := true
 
-# Encryption
-PLATFORM_SDK_VERSION := 28
-TW_INCLUDE_CRYPTO := true
-TW_INCLUDE_CRYPTO_FBE := true
-TW_CRYPTO_USE_SYSTEM_VOLD := false
-TARGET_HW_DISK_ENCRYPTION := false
-TARGET_KEYMASTER_WAIT_FOR_QSEE := true
-
-# Hack: prevent anti rollback
-PLATFORM_SECURITY_PATCH := 2099-12-31
-VENDOR_SECURITY_PATCH := 2099-12-31
+# Inherit proprietary version
+-include vendor/xiaomi/pine/BoardConfigVendor.mk
--- vendorsetup.sh
+++ vendorsetup.sh
@@ -14,4 +14,4 @@
 # limitations under the License.
 #
 
-add_lunch_combo omni_pine-eng
+add_lunch_combo lineage_pine-userdebug
